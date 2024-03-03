635538404a1324b4123e3e422e32a12b1e2491a7
Sophie Boyer <scboyer.dev@gmail.com>
2024-03-03 22:03:53 +0100

    update session dates

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -44,10 +44,7 @@ export default function HomePage() {
       </ul>
       <br />
       <br />
-      <strong>
-        Inscriptions ouvertes pour la prochaine session du 3 mars au 21 avril
-        2024, limitée à 15 participants.
-      </strong>
+      <strong>Prochaine session pour début mai.</strong>
     </main>
   );
 }
--- src/pages/ParticipantsPage.jsx
+++ src/pages/ParticipantsPage.jsx
@@ -5,21 +5,26 @@ import "../assets/CSS/ParticipantsPage.css";
 
 export default function ParticipantsPage() {
   const [authorsRegistered, setAuthorsRegistered] = useState();
+  const [authorsActive, setAuthorsActive] = useState();
   const [isloading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const getAuthorsRegistered = async () => {
+    const getAuthors = async () => {
       try {
-        const { data } = await axios.get(
+        const registered = await axios.get(
           "https://site--entrauteurs-backend--dzk9mdcz57cb.code.run/authors/Registered"
         );
-        setAuthorsRegistered(data);
+        setAuthorsRegistered(registered.data);
+        const active = await axios.get(
+          "https://site--entrauteurs-backend--dzk9mdcz57cb.code.run/authors/Active"
+        );
+        setAuthorsActive(active.data);
         setIsLoading(false);
       } catch (error) {
         console.log(error.message);
       }
     };
-    getAuthorsRegistered();
+    getAuthors();
   }, []);
 
   return isloading ? (
@@ -29,12 +34,34 @@ export default function ParticipantsPage() {
       <h2>Liste des participants inscrits pour la prochaine session.</h2>
       <br />
       <p>
-        Du 3 mars au 21 avril 2024 : inscriptions momentanément fermées pour la
-        prochaine session.
+        La session commencera début mai. Les inscriptions ouvriront fin
+        mars/début avril. Rejoins le Discord pour ne manquer aucune info !
       </p>
       <br />
       <div className="authorsContainer">
         {authorsRegistered.authors.map((author, index) => {
+          return (
+            <div key={index}>
+              <div>
+                <strong>{author.username}</strong>
+              </div>
+              <div>{author.story_title}</div>
+              <div>
+                <a href={author.story_url} target="_blank">
+                  <img src={author.story_cover} alt={author.story_title} />
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <br />
+      <h2>Liste des participants inscrits pour la session en cours.</h2>
+      <br />
+      <p>Du 3 mars au 21 avril 2024.</p>
+      <br />
+      <div className="authorsContainer">
+        {authorsActive.authors.map((author, index) => {
           return (
             <div key={index}>
               <div>
